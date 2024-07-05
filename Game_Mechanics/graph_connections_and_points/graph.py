77b94cf9d13364cac13c88084cc73fbd3d96d1ab
Samuel Golovin <samuel.samyco@gmail.com>
2024-07-05 16:58:09 -0500

    working on using nodes as dictionary names

--- Game_Mechanics/graph_connections_and_points/graph.py
+++ Game_Mechanics/graph_connections_and_points/graph.py
@@ -1,11 +1,5 @@
-import pygame
-import sys
+import heapq
 import math
-
-pygame.init()
-screen_width, screen_height = 800, 600
-screen = pygame.display.set_mode((screen_width, screen_height))
-pygame.display.set_caption("Game Mechanic Test")
 
 class Node:
     def __init__(self, position, type):
@@ -16,9 +10,9 @@ class Graph:
     def __init__(self):
         self.graph = {}
 
-    def add_node(self, node, node_location, node_type):
+    def add_node(self, node):
         if node not in self.graph:
-            self.graph[node] = [Node(node_location, node_type)]
+            self.graph[node] = []
 
     def add_connection(self, node1, node2):
         if node1 in self.graph and node2 in self.graph:
@@ -27,13 +21,39 @@ class Graph:
         else:
             print("Error: Both nodes must exist in the graph")
 
+    def remove_connection(self, node1, node2):
+        if node1 in self.graph and node2 in self.graph:
+            self.graph[node1].remove(node2)
+            self.graph[node2].remove(node1)
+        else:
+            print("Error: Both nodes must exist in the graph")
+
     def get_nodes(self):
         return list(self.graph.keys())
     
     def get_connections(self, node):
         if node in self.graph:
-            return self.graph[node][1:]
+            return self.graph[node]#[1:]
         else:
             print(f"Error: {node} does not exist in the graph.")
             return []
         
+    def calculate_distance(self, node1, node2):
+        x1, y1 = self.graph[node1].position
+        x2, y2 = self.graph[node2].position
+        return math.sqrt((x2 - x1)**2 + (y2 - y1)**2)
+        
+    def pathfinding(self, start, end):
+        queue = [(0, start, [])]
+        seen = set()
+        while queue:
+            (cost, node, path) = heapq.heappop(queue)
+            if node not in seen:
+                path = path + [node]
+                seen.add(node)
+                if node == end:
+                    return path
+                for connected_node in self.get_connections(node):
+                    total_cost = cost + self.calculate_distance(node, connected_node)
+                    heapq.heappush(queue, (total_cost, connected_node, path))
+        return []
--- Game_Mechanics/graph_connections_and_points/main.py
+++ Game_Mechanics/graph_connections_and_points/main.py
@@ -2,6 +2,7 @@ import pygame
 import sys
 
 from graph import Graph
+from graph import Node
 
 pygame.init()
 screen_width, screen_height = 800, 600
@@ -10,35 +11,46 @@ pygame.display.set_caption("Game Mechanic Test")
 
 city_graph = Graph()
 
-city_graph.add_node('A', (200, 200), 'Basic')
-city_graph.add_node('B', (100, 200), 'Super')
-city_graph.add_node('C', (300, 100), 'Extra')
+node1 = Node((100, 100), 'A')
+node2 = Node((200, 100), 'B')
+node3 = Node((300, 200), 'C')
+node4 = Node((400, 300), 'D')
 
-for node in city_graph.graph:
-    print(node)
-    print(city_graph.graph[node][0].position)
+node5 = Node((150 + 100, 150 + 100), 'A1')
+node6 = Node((250 + 100, 150 + 100), 'B1')
+node7 = Node((350 + 100, 250 + 100), 'C1')
+node8 = Node((450 + 100, 350 + 100), 'D1')
+
+city_graph.add_node(node1)
+city_graph.add_node(node2)
+city_graph.add_node(node3)
+city_graph.add_node(node4)
+city_graph.add_node(node5)
+city_graph.add_node(node6)
+city_graph.add_node(node7)
+city_graph.add_node(node8)
+
+
+city_graph.add_connection(node1, node2)
+city_graph.add_connection(node2, node3)
+city_graph.add_connection(node3, node4)
+city_graph.add_connection(node4, node5)
 
 # print(city_graph.graph)
-# print(city_graph.get_nodes())
-
-city_graph.add_connection('A', 'B')
-city_graph.add_connection('B', 'C')
-# print(city_graph.graph)
-
-# print(city_graph.get_connections('B'))
 
 
 def draw_city(city_graph):
     for node in city_graph.graph:
         pygame.draw.circle(screen, (255, 0, 0), city_graph.graph[node][0].position, 15)
         font = pygame.font.Font(None, 20)
-        text_surface = font.render(node, True, (0, 0, 0))
+        text_surface = font.render(city_graph.graph[node][0].type, True, (0, 0, 0))
         text_rect = text_surface.get_rect(center = city_graph.graph[node][0].position)
         screen.blit(text_surface, text_rect)
 
-    for node1 in city_graph.get_nodes():
-        for node2 in city_graph.get_connections(node1):
-            pygame.draw.line(screen, (0, 0, 255), city_graph.graph[node1][0].position, city_graph.graph[node2][0].position)
+    for node in city_graph.get_nodes():
+        for connected_node in city_graph.get_connections(node):
+            pygame.draw.line(screen, (0, 0, 255), city_graph.graph[node][0].position, city_graph.graph[connected_node][0].position)
+
 
 while True:
     for event in pygame.event.get():
